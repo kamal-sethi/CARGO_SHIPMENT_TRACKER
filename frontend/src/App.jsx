03e7743ab21d5b568eb64e404c169e18d77a2f16
kamal-sethi <kamalsethi018@gmail.com>
2025-03-08 18:32:42 +0530

    created a folder structure for creating ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
