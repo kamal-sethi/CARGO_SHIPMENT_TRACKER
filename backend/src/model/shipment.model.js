6d8b4193493c4ef5fe37fc83e9291e7506eb26c7
kamal-sethi <kamalsethi018@gmail.com>
2025-03-08 16:07:07 +0530

    created a model for shipment

---
+++ backend/src/model/shipment.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+const shipmentSchema = new mongoose.Schema(
+  {
+    shipmentName: {
+      type: String,
+      required: true,
+    },
+    shipmentId: {
+      type: Number,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    containerId: {
+      type: Number,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    locations: {
+      type: [String],
+      required: true,
+      lowercase: true,
+    },
+    ETA: {
+      type: Date,
+      required: true,
+      default: null,
+    },
+    currentLocation: {
+      type: String,
+      default: "Not Updated",
+    },
+    shipmentHead: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Not updated", "delivered", "pending", "cancelled", "In transit"],
+      default: "Not updated",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Shipment = mongoose.model("Shipment", shipmentSchema);
+export default Shipment;
